5242643748e62ef26ffc572c090bb7bb29eebd40
Ashok Giri <105265254+sautaboy@users.noreply.github.com>
2022-11-07 22:13:42 +0545

    Create functionExpression.js

---
+++ functionExpression.js
@@ -0,0 +1,54 @@
+// function sayHi() {
+    //     alert("Hello Maya")
+    // }
+    // sayHi()
+
+    // let sayHi = function () {
+    //     alert("Hello Maya")
+    // }
+    // let func=sayHi
+    // sayHi()
+    // func()
+
+
+
+    // function ask(questtion, yes, no) {
+    //     if (confirm(questtion)) {
+    //         yes()
+    //     }
+    //     else {
+    //         no()
+    //     }
+    // }
+    // function ok() {
+    //     alert("You Agreed")
+    // }
+    // function cancel() {
+    //     alert("You Canceled")
+    // }
+    // ask(`Are You Interested In Pornography ?`, ok, cancel)
+
+
+    // function ask(questtion, yes, no) {
+    //     if (confirm(questtion)) yes()
+    //     else no()
+    // }
+    // ask("Do You Love Me?",
+    //     function () { alert("I love You Too") },
+    //     function () { alert("I hate You Madarchod") }
+    // )
+
+
+    
+    // let age = prompt("enter Your Age", 18)
+    // if (age >= 18) {
+    //     function welcome() {
+    //         alert("Welcome")
+    //     }
+    // }
+    // else {
+    //     function welcome() {
+    //         alert("Bye Bye")
+    //     }
+    // }
+    // welcome()
