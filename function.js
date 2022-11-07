2e99e7af52f4145f7860d47abd5b02b69aaa521e
Ashok Giri <105265254+sautaboy@users.noreply.github.com>
2022-11-07 21:38:44 +0545

    Create function.js

---
+++ function.js
@@ -0,0 +1,89 @@
+// let userName = "Ashok Giri"
+// function showMessage() {
+//     console.log(`Hello ${userName}`)
+// }
+// showMessage()
+
+
+// let userName = "Ashok Giri"
+// function showMessage() {
+//     userName = "Sauta Boy"
+//     console.log(`HeFllo ${userName}`)
+// }
+// console.log(userName)
+// showMessage()
+// console.log(userName)
+
+
+
+// function showMessage(from, next, prev) {
+//     console.log(from, next, prev)
+// }
+// showMessage("Hello", "Sauta", "Boy")
+
+
+
+// function showMessage(from, text = "Mr.Sauta Boy, What Are You Doing?") {
+//     console.log(`${from} ${text}`)
+// }
+// showMessage("Hello")
+
+// function showMessage(from, text = `Tero Ghar Kata Ho ?`) {
+//     console.log(from, text)
+// }
+// showMessage("Randi Ko Xoro ")
+
+
+
+// function showMessage(a, b) {
+//     sum = a + b
+//     sub = a - b
+//     mul = a * b
+//     div = a / b
+// }
+// showMessage(5,5)
+// console.log(sum)
+// console.log(sub)
+// console.log(mul)
+// console.log(div)
+
+
+
+
+// function checkAge(age) {
+//     console.log(age)
+//     if (age >= 18 && age < 110) {
+//         console.log(`You Age Is ${age}:You Are Qualified`)
+//     }
+//     else {
+//         if (age <= 0) {
+//             console.log(`Please Enter a valid Age`)
+//         }
+//         else {
+//             if (age > 110) {
+//                 console.log("You Are Already Dead Mother Fucker")
+//             }
+//             else {
+//                 if (age < 18) {
+//                     console.log(`Your Age is ${age}: You Are Not Qualified`)
+//                 }
+//             }
+//         }
+//     }
+
+// }
+// checkAge(69)
+
+// function min(a, b) {
+//     if (a < b) {
+//         console.log(a)
+//     }
+//     else
+//         console.log(b)
+// }
+// min(8,6)
+
+// function min(a, b) {
+//     console.log(a < b ? a : b)
+// }
+// min(4, 2)
