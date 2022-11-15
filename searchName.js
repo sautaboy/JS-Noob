9cb4b7875459208ea72ee137ffe0de938b44c75f
Ashok Giri <105265254+sautaboy@users.noreply.github.com>
2022-11-15 21:03:02 +0545

    Create searchName.js

---
+++ searchName.js
@@ -0,0 +1,33 @@
+
+
+// This will Tell the Defination
+console.log("Dictionary Not Found")
+
+let userName =
+        [
+            Ashok = "He is A Good Boy",
+            Sauta = "He is Also a Good Boy",
+            Kauro = "he is Very Talented Boy",
+            Kaley = "He a Very Black",
+            Balxi = "He Does Fishing",
+
+        ]
+    let userSearch = prompt("Enter The name")
+    if (userSearch == "Ashok" || userSearch == "ashok") {
+        console.log(Ashok)
+    }
+    else if (userSearch == "Sauta" || userSearch == "sauta") {
+        console.log(Sauta)
+    }
+    else if (userSearch == "Kauro" || userSearch == "kauro") {
+        console.log(Kauro)
+    }
+    else if (userSearch == "Kaley" || userSearch == "kaley") {
+        console.log(Kaley)
+    }
+    else if (userSearch == "Balxi" || userSearch == "balxi") {
+        console.log(Balxi)
+    }
+    else {
+        console.log("Name Not Found")
+    }
