b93a8e8cf4f07692b50808603865633f8b22c868
Ashok Giri <105265254+sautaboy@users.noreply.github.com>
2022-11-18 19:01:38 +0545

    Create forInLoop.js

---
+++ Array/forInLoop.js
@@ -0,0 +1,11 @@
+<script>
+
+    // In the for in lop it will show the Whole Array elements location or address like in 0 there is Apple and in 2 Banana
+    let fruits = ['Apple', 'Banana', 'Mango', 'orange', 'Kera'];
+    fruits.push("hello");
+    let fruits1 = [];
+    for (let fruit in fruits) {
+        console.log(fruit); // it will show the index number of the fruits elements
+        console.log(fruits[fruit]); //it will show the fruits element and fruits will show the Index Of that Elements
+    }
+</script>
